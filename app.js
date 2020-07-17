83595ecc79a78c4c9b7cf452517addff350f6837
Natiq Mumtaz <natiqkhan00@gmail.com>
2020-07-18 00:33:52 +0200

    ENV Port

--- app.js
+++ app.js
@@ -16,4 +16,4 @@ app.use(bodyParser.json())
 app.use(require('./routes/index-routes'))
 
 
-app.listen('3000', () => console.log('Server running on port 3000'));
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
